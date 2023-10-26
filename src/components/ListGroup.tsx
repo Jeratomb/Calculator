eb701b89061566b2e3950b3414738e876fc2a1c3
Jeratomb <rayen.phillip@googlemail.com>
2023-10-26 13:51:33 +0200

    logic of calc is done, dark mode needs fixing

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,36 +1,30 @@
 import { useState } from "react";
 
-interface ListGroupProps {
-  items: string[];
-  heading: string;
-  onSelectItem: (item: string) => void;
-}
+function ListGroup() {
+  let items = ["New York", "Berlin", "San Francisco", "London"];
 
-function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>{heading}</h1>
-      {items && items.length === 0 && <p>No item found</p>}
+      <h1>List</h1>
+      {items.length === 0 && <p>No Item found</p>}
       <ul className="list-group">
-        {items &&
-          items.map((item, index) => (
-            <li
-              className={
-                selectedIndex === index
-                  ? "list-group-item active"
-                  : "list-group-item"
-              }
-              key={item}
-              onClick={() => {
-                setSelectedIndex(index);
-                onSelectItem(item);
-              }}
-            >
-              {item}
-            </li>
-          ))}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
